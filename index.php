1a87a24fe0c57dac18bdddf0b182583753a00ace
Roie Cohen <roie.cohen@gmail.com>
2012-02-18 23:51:28 +0200

    Some fixes

--- index.php
+++ index.php
@@ -14,8 +14,10 @@
 		<h3>Enviornment: <?=$conf['env_name']?></h3>
 		<h4><a href="<?=$conf['base_switch']?>">Switch Enviorment</a></h6>
 			<ul>
-				<li><a href="<?php echo $confp['base'] . "/crm/index.php"; ?>">CRM</a></li>
-				<li><a href="<?php echo $confp['base'] . "/install/index.php"; ?>">Install</a></li>
+				<li><a href="<?php echo $confp['base'] . "tools/crm/index.php"; ?>">CRM</a></li>
+                <li><a href="<?php echo $confp['base'] . "tools/crms/index.php"; ?>">CRMs</a></li>
+                <li><a href="<?php echo $confp['base'] . "tools/fxp_mapping/index.php"; ?>">CRMs</a></li>
+                <li><a href="<?php echo $confp['base'] . "tools/like_tag_mapping/index.php"; ?>">CRMs</a></li>
 			</ul>
 	</body>
 </html>
